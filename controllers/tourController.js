f0b92e9ecdda79093f8d2d6b513fdd4e0c1d6420
paradoxo <vinicius_baggio@hotmail.com>
2023-09-27 22:10:15 -0300

    refactor(Tours): Refactoring API Features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
-const parseFilterQuery = require("../helpers/parseQueryFilter");
 const Tour = require("../models/tourModel");
+const apiFeatures = require("../helpers/apiFeatures");
 
 /**
  * @param {IncomingMessage} req
@@ -7,28 +7,7 @@ const Tour = require("../models/tourModel");
  * */
 const getAllTours = async (req, res) => {
     try {
-        const queryFind = parseFilterQuery(Tour, req.query);
-        let query = Tour.find(queryFind);
-
-        const sort = req.query.sort || "-createdAt";
-        query = query.sort(sort.replaceAll(",", " "));
-
-        const fields = req.query.fields || "-__v";
-        query = query.select(fields.replaceAll(",", " "));
-
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const total = await Tour.countDocuments();
-            if (skip >= total) {
-                throw new Error("This page does not exist");
-            }
-        }
-
-        const tours = await query.exec();
+        const tours = await new apiFeatures(Tour, req.query).all().exec();
 
         return res.status(200).json({
             status: "success",
@@ -36,6 +15,7 @@ const getAllTours = async (req, res) => {
             data: { tours },
         });
     } catch (error) {
+        console.log(error);
         return res.status(400).json({ status: "fail", message: error });
     }
 };
---
+++ helpers/apiFeatures.js
@@ -0,0 +1,90 @@
+module.exports = class {
+    #query;
+
+    constructor(
+        model,
+        queryBody,
+        { sort = "-createdAt", fields = "-__v", page = 1, limit = 100 } = {},
+    ) {
+        this.model = model;
+        this.query = queryBody;
+        this.defaultSort = sort;
+        this.defaultFields = fields;
+        this.defaultPage = page;
+        this.defaultLimit = limit;
+        this.#query = this.model.find();
+    }
+
+    #getModelPaths() {
+        return Object.keys(this.model.schema.paths);
+    }
+
+    #parseFilterQueryValue(queryValue) {
+        if (Array.isArray(queryValue)) {
+            queryValue = queryValue.at(-1);
+        }
+
+        if (typeof queryValue === "object") {
+            for (const [key, value] of Object.entries(queryValue)) {
+                queryValue[`$${key}`] = value;
+                delete queryValue[key];
+            }
+        }
+
+        return queryValue;
+    }
+
+    #parseFilterQuery() {
+        if (Object.keys(this.query).length === 0) {
+            return {};
+        }
+
+        const filteredQuery = {};
+
+        for (const path of this.#getModelPaths()) {
+            if (this.query[path] != null) {
+                filteredQuery[path] = this.#parseFilterQueryValue(
+                    this.query[path],
+                );
+            }
+        }
+
+        return filteredQuery;
+    }
+
+    filter() {
+        this.#query = this.#query.find(this.#parseFilterQuery());
+        return this;
+    }
+
+    sort() {
+        let sortString = this.query.sort || this.defaultSort;
+        sortString = sortString.replaceAll(",", " ");
+        this.#query = this.#query.sort(sortString);
+        return this;
+    }
+
+    select() {
+        let fieldsString = this.query.fields || this.defaultFields;
+        fieldsString = fieldsString.replaceAll(",", " ");
+        this.#query = this.#query.select(fieldsString);
+        return this;
+    }
+
+    pagination() {
+        const page = this.query.page * 1 || this.defaultPage;
+        const limit = this.query.limit * 1 || this.defaultLimit;
+        const skip = (page - 1) * limit;
+
+        this.#query = this.#query.skip(skip).limit(limit);
+        return this;
+    }
+
+    all() {
+        return this.filter().sort().select().pagination();
+    }
+
+    exec() {
+        return this.#query.exec();
+    }
+};
