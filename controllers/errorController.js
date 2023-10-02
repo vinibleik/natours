132aae433a39f4c4db4804f34b38620fbb788e64
paradoxo <vinicius_baggio@hotmail.com>
2023-10-01 21:39:34 -0300

    feat(Error): Handle error prod x dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,38 @@
-const errorHandler = (err, _req, res, _next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
-
+const sendErrorDev = (err, res) => {
     return res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
+        error: err,
+        stack: err.stack,
     });
 };
 
-module.exports = errorHandler;
+const sendErrorProd = (err, res) => {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+        return res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    }
+
+    // Programming or other unknown error: don't leak error details
+    console.error(err);
+    return res.status(500).json({
+        status: "error",
+        message: "Something went very wrong!",
+    });
+};
+
+const globalErrorHandler = (err, _req, res, _next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    if (process.env.NODE_ENV === "development") {
+        return sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === "production") {
+        return sendErrorProd(err, res);
+    }
+};
+
+module.exports = globalErrorHandler;
