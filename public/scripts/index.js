cef086a1c36b02b0629d39b9948f1a82e29ed326
paradoxo <vinicius_baggio@hotmail.com>
2023-11-01 12:56:53 -0300

    feat(Frontend): Update user password via API

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,5 @@
 import { login, logout } from "./login.js";
-import { update } from "./update.js";
+import { updateData } from "./update.js";
 
 const form = document.querySelector(".form--login");
 if (form) {
@@ -16,12 +16,41 @@ if (logoutBtn) {
     logoutBtn.addEventListener("click", logout);
 }
 
-const formUpdate = document.querySelector(".form-user-data");
-if (formUpdate) {
-    formUpdate.addEventListener("submit", function (e) {
+const formUpdateData = document.querySelector(".form-user-data");
+if (formUpdateData) {
+    formUpdateData.addEventListener("submit", function (e) {
         e.preventDefault();
+        const btn = document.getElementById("btn-save-data");
+        const previousValue = btn.textContent;
+        btn.textContent = "Updating...";
+        btn.disabled = true;
         const formData = new FormData(e.target);
-        console.log(formData);
-        update(formData.get("name"), formData.get("email"));
+        updateData("user-data", {
+            name: formData.get("name"),
+            email: formData.get("email"),
+        }).then(() => {
+            btn.textContent = previousValue;
+            btn.disabled = false;
+        });
+    });
+}
+
+const formUpdatePassword = document.querySelector(".form-user-settings");
+if (formUpdatePassword) {
+    formUpdatePassword.addEventListener("submit", function (e) {
+        const btn = document.getElementById("btn-save-password");
+        const previousValue = btn.textContent;
+        btn.textContent = "Updating...";
+        btn.disabled = true;
+        e.preventDefault();
+        const formData = new FormData(formUpdatePassword);
+        updateData("password", {
+            curPassword: formData.get("curPassword"),
+            newPassword: formData.get("newPassword"),
+            newPasswordConfirm: formData.get("newPasswordConfirm"),
+        }).then(() => {
+            btn.textContent = previousValue;
+            btn.disabled = false;
+        });
     });
 }
--- public/scripts/update.js
+++ public/scripts/update.js
@@ -1,13 +1,21 @@
 import { showAlert } from "./alerts.js";
 
-export async function update(name, email) {
+/**
+ * @typedef {Object} UserData
+ * @property {string} name - Name of the user
+ * @property {string} email - Email of the user
+ *
+ * @typedef {Object} UserPassword
+ * @property {string} curPassword - Current user's password
+ * @property {string} newPassword - New password for the user
+ * @property {string} newPasswordConfirm - New password confirm for the user
+ * */
+
+async function update(url, method, body) {
     try {
-        const response = await fetch("/api/v1/users/update-me", {
-            method: "PUT",
-            body: JSON.stringify({
-                name,
-                email,
-            }),
+        const response = await fetch(url, {
+            method: method,
+            body: JSON.stringify(body),
             headers: { "Content-Type": "application/json" },
         });
         const data = await response.json();
@@ -17,10 +25,44 @@ export async function update(name, email) {
                 location.reload();
             }, 500);
         } else {
-            showAlert("error", "Invalid name or email!");
+            showAlert("error", data.message);
         }
     } catch (err) {
-        console.error("Network Error!");
+        showAlert("error", "Network Error!");
         console.error(err);
+    }
+}
+
+function updatePassword(curPassword, newPassword, newPasswordConfirm) {
+    return update("/api/v1/users/update-password", "PATCH", {
+        curPassword,
+        newPassword,
+        newPasswordConfirm,
+    });
+}
+
+function updateUserData(name, email) {
+    return update("/api/v1/users/update-me", "PUT", {
+        name,
+        email,
+    });
+}
+
+/**
+ * @param {("user-data" | "password")} type
+ * @param {UserData | UserPassword} body
+ * */
+export async function updateData(type, body) {
+    switch (type) {
+        case "user-data":
+            return await updateUserData(body.name, body.email);
+        case "password":
+            return await updatePassword(
+                body.curPassword,
+                body.newPassword,
+                body.newPasswordConfirm,
+            );
+        default:
+            return showAlert("error", "Invalid update!");
     }
 }
